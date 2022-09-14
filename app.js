fe0000d4348f98ebd5c0c18aef98d919959c5f83
MuratÇamurlu <mcamurlu1923@gmailcom>
2022-09-14 12:51:23 +0300

    emojiler yerleştirildi

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const randomNumber = Math.round(Math.random() * 100);
 console.log(randomNumber);
+let score = 10;
+let topScore = 0;
 document.querySelector(".check-btn").addEventListener("click", () => {
   const guessInput = Number(document.querySelector(".guess-input").value);
   const msg = document.querySelector(".msg");
+  const body = document.querySelector("body");
   if (!guessInput) {
     msg.innerText = "Please enter a number";
   } else if (randomNumber === guessInput) {
-    msg.innerText = "Congrats You Win";
+    msg.innerHTML = `Congrats You Win <i class="fa-solid fa-face-grin-stars fa-2x"></i>`;
+    body.className = "bg-success";
+    if (score > topScore) {
+      topScore = score;
+      document.querySelector(".top-score").textContent = topScore;
+    }
+    document.querySelector(".secret-number").textContent = randomNumber;
+  } else {
+    if (--score > 0) {
+      guessInput > randomNumber
+        ? (msg.innerHTML = `DECREASE <i class="fa-solid fa-arrow-trend-down fa-2x"></i>`)
+        : (msg.innerHTML = `INCREASE <i class="fa-solid fa-arrow-trend-up fa-2x"></i>`);
+    } else {
+      msg.innerHTML = `You Lost <i class="fa-solid fa-face-sad-cry fa-2x"></i>`;
+      document.querySelector(".check-btn").disabled = true;
+      body.className = "bg-danger";
+    }
+
+    document.querySelector(".score").textContent = score;
   }
 });
