ce22f77a1a657b3e4c81261b07747eff3e562528
MuratÇamurlu <mcamurlu1923@gmailcom>
2022-09-14 14:08:56 +0300

    part of again completed

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const randomNumber = Math.round(Math.random() * 100);
 console.log(randomNumber);
 let score = 10;
-let topScore = 0;
+// let topScore = 0;
+let topScore = localStorage.getItem("topScore") || 0;
+document.querySelector(".top-score").textContent = topScore;
+
 document.querySelector(".check-btn").addEventListener("click", () => {
   const guessInput = Number(document.querySelector(".guess-input").value);
   const msg = document.querySelector(".msg");
@@ -11,9 +14,9 @@ document.querySelector(".check-btn").addEventListener("click", () => {
   } else if (randomNumber === guessInput) {
     msg.innerHTML = `Congrats You Win <i class="fa-solid fa-face-grin-stars fa-2x"></i>`;
     body.className = "bg-success";
-    if (score > topScore) {
-      topScore = score;
-      document.querySelector(".top-score").textContent = topScore;
+    if (score >= topScore) {
+      localStorage.setItem("topScore", score);
+      document.querySelector(".top-score").textContent = score;
     }
     document.querySelector(".secret-number").textContent = randomNumber;
   } else {
@@ -23,10 +26,23 @@ document.querySelector(".check-btn").addEventListener("click", () => {
         : (msg.innerHTML = `INCREASE <i class="fa-solid fa-arrow-trend-up fa-2x"></i>`);
     } else {
       msg.innerHTML = `You Lost <i class="fa-solid fa-face-sad-cry fa-2x"></i>`;
+      document.querySelector(".secret-number").textContent = randomNumber;
       document.querySelector(".check-btn").disabled = true;
       body.className = "bg-danger";
     }
 
     document.querySelector(".score").textContent = score;
   }
+});
+
+document.querySelector(".again-btn").addEventListener(`click`, () => {
+  score = 10;
+  document.querySelector(".score").textContent = score;
+  const randomNumber = Math.round(Math.random() * 100);
+  console.log(randomNumber);
+  document.querySelector(".check-btn").disabled = false;
+  document.querySelector(".secret-number").textContent = "?";
+  document.querySelector("body").className = " #2d3436";
+  document.querySelector(".guess-input").value = "";
+  document.querySelector(".msg").textContent = "Starting";
 });
